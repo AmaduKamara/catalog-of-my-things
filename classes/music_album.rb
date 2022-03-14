827d50caf117bbb64825c46b6e16581591ac3d50
Ewoskyx <ewoskyx@gmail.com>
2022-03-14 23:31:21 +0300

    Fix linters

--- classes/music_album.rb
+++
@@ -1,8 +0,0 @@
-require './item'
-attr_accessor: :on_spotify
-class MusicAlbum < Item
-    def initialize(id: nil, on_spotify: false)
-        @id = id || Random.rand(1..500)
-        @on_spotify = on_spotify
-    end
-end
