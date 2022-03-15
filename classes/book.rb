605e51d9717eef4482068f8d75298f916de5f36d
Buyaki <rittahbuyaki@gmail.com>
2022-03-15 16:08:12 +0300

    Implement methods

--- classes/book.rb
+++ classes/book.rb
@@ -1,11 +1,18 @@
 class Book < Item
+  attr_accessor :publisher, :cover_state, :items
+
   def initialize
     @publisher = publisher
     @cover_state = cover_state
+    @items = []
     super
   end
 
+  def add_item(publish_date, archived, label)
+    Item.new(publish_date, archived, self, label)
+  end
+
   def can_be_archived?(item)
-    item.can_be_archived? == true
+    item.can_be_archived? == true || cover_state == 'bad'
   end
 end
--- classes/label.rb
+++ classes/label.rb
@@ -1,8 +1,14 @@
 class Label < Item
+  attr_accessor :title, :color, :items
+
   def initialize(title, color)
     @title = title
     @color = color
-    # items = []
+    @items = []
     super
+  end
+
+  def add_item(publish_date, archived, book)
+    Item.new(publish_date, archived, book, self)
   end
 end
