7c7230947c7be62c3ad580b6daa3c565797e80f4
Amkam <amaduskamara@gmail.com>
2022-03-15 12:12:51 +0000

    Add class properties and methods for Game and Author classes

--- classes/author.rb
+++ classes/author.rb
@@ -1,3 +1,18 @@
-class Author
+require './item.rb'
 
+class Author < Item
+  attr_accessor :first_name, :last_name
+
+  initialize(id, first_name, last_name)
+    @id = id
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+
+  item = Item.new()
+
+  def add_item=(item)
+    items.push(item)
+  end
 end
--- classes/game.rb
+++ classes/game.rb
@@ -1,3 +1,22 @@
-class Game
+require 'date'
+require './item.rb'
 
+class Game < Item
+  attr_accessor :multiplayer, :last_played_at
+
+  initialize(multiplayer, last_played_at)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+    @can_be_archived = can_be_archived
+  end
+
+  item = Item.new
+
+  def can_be_archived?
+    if item.can_be_archived && last_played_at > 2
+      true
+    else
+      false
+    end
+  end
 end
