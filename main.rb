a58f822056edd70adb2a89d847bb0e7d3912cd16
Ewoskyx <ewoskyx@gmail.com>
2022-03-16 21:21:59 +0300

    Add music&genre options + music&genre json storage

--- main.rb
+++ main.rb
@@ -1,55 +1,24 @@
-require './classes/handle_book'
+require './app'
 
-@handle_book = HandleBooks.new
+class Main
+  def initialize
+    @app = App.new
+  end
 
-def menu
-  puts 'Please choose an option by entering a number: '
-  puts '1 - List all books'
-  puts '2 - List all music albums'
-  puts '3 - List of games'
-  puts '4 - List all genres'
-  puts '5 - List all labels'
-  puts '6 - List all authors'
-  puts '7 - Add a book'
-  puts '8 - Add a music album'
-  puts '9 - Add a game'
-  puts '10 - Exit'
-end
-
-# rubocop:disable Metrics/CyclomaticComplexity
-def choose_options(user_input)
-  case user_input
-  when 1
-    @handle_book.display_books
-  when 2
-    display_music albums
-  when 3
-    display_games
-  when 4
-    display_genres
-  when 5
-    @handle_book.display_labels
-  when 6
-    display_authors
-  when 7
-    @handle_book.create_book
-  when 8
-    create_musicAlbum
-  when 9
-    create game
+  def run
+    puts '**********   Welcome to our App   ***********'.black.on_white.bold
+    is_working = true
+    while is_working == true
+      @app.menu
+      user_input = gets.chomp.to_i
+      if user_input == 10
+        @app.save_data
+        is_working = false
+      end
+      @app.choose_options(user_input)
+    end
   end
 end
 
-# rubocop:enable Metrics/CyclomaticComplexity
-def main()
-  puts 'Welcome to our App'
-  is_working = true
-  while is_working == true
-    menu
-    user_input = gets.chomp.to_i
-    is_working = false if user_input == 10
-    choose_options(user_input)
-  end
-end
-
-main
+main = Main.new
+main.run
