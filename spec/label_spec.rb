1f2050c178b8f3a7e66bb4924c373035db9f4a61
Buyaki <rittahbuyaki@gmail.com>
2022-03-17 10:09:24 +0300

    Fix rspec error

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -5,7 +5,7 @@ require 'date'
 describe Label do
   context 'It should create a Label' do
     newitem = Item.new(id: nil, publish_date: Date.parse('1999/10/26'))
-    label = Label.new(id: 1, title: 'Gift', color: 'Red')
+    label = Label.new(title: 'Gift', color: 'Red')
     it 'should create a new label' do
       expect(label.title).to eq 'Gift'
     end
